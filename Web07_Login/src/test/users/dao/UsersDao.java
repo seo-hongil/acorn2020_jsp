5e1013323e04d52657d77292c3e7586df3a94844
hongil <shi8295@naver.com>
2020-07-17 14:55:40 +0900

    web07_login 이미지 기능 추가

--- Web07_Login/src/test/users/dao/UsersDao.java
+++ Web07_Login/src/test/users/dao/UsersDao.java
@@ -17,44 +17,45 @@ public class UsersDao {
 		return dao;
 	}
 	//인자로 전달된 아이디가 users 테이블에 존재하는지 여부를 리턴하는 메소드
-		public boolean isExist(String inputId) {
-			
-			boolean isExist=false;
-			//필요한 객체의 참조값을 담을 지역변수 만들기 
-			Connection conn = null;
-			PreparedStatement pstmt = null;
-			ResultSet rs = null;
-			try {
-				//Connection 객체의 참조값 얻어오기 
-				conn = new DbcpBean().getConn();
-				//실행할 sql 문 준비하기
-				String sql = "SELECT id"
-						+ " FROM users"
-						+ " WHERE id=?";
-				pstmt = conn.prepareStatement(sql);
-				//sql 문에 ? 에 바인딩할 값이 있으면 바인딩하고 
-				pstmt.setString(1, inputId);
-				//select 문 수행하고 결과 받아오기 
-				rs = pstmt.executeQuery();
-				//select 된 row 가 있는지 확인한다. 
-				if (rs.next()) {
-					isExist=true;//해당 아이디가 이미 존재 하는 경우 
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
-			} finally {
-				try {
-					if (rs != null)
-						rs.close();
-					if (pstmt != null)
-						pstmt.close();
-					if (conn != null)
-						conn.close();
-				} catch (Exception e) {
-				}
+	public boolean isExist(String inputId) {
+		
+		boolean isExist=false;
+		//필요한 객체의 참조값을 담을 지역변수 만들기 
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			//Connection 객체의 참조값 얻어오기 
+			conn = new DbcpBean().getConn();
+			//실행할 sql 문 준비하기
+			String sql = "SELECT id"
+					+ " FROM users"
+					+ " WHERE id=?";
+			pstmt = conn.prepareStatement(sql);
+			//sql 문에 ? 에 바인딩할 값이 있으면 바인딩하고 
+			pstmt.setString(1, inputId);
+			//select 문 수행하고 결과 받아오기 
+			rs = pstmt.executeQuery();
+			//select 된 row 가 있는지 확인한다. 
+			if (rs.next()) {
+				isExist=true;//해당 아이디가 이미 존재 하는 경우 
 			}
-			return isExist; //아이디 존재 여부를 리턴한다. 
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (Exception e) {
+			}
+		}
+		return isExist; //아이디 존재 여부를 리턴한다. 
 	}
+
 	//회원정보(이메일)를 수정 반영하는 메소드
 	public boolean update(UsersDto dto) {
 		Connection conn = null;
@@ -62,15 +63,16 @@ public class UsersDao {
 		int flag = 0;
 		try {
 			conn = new DbcpBean().getConn();
-			//실행할 sql 문 준비하기
+			//실행할 sql 문 준비하기 
 			String sql = "UPDATE users"
-					+ " SET email=?"
+					+ " SET email=?, profile=?"
 					+ " WHERE id=?";
 			pstmt = conn.prepareStatement(sql);
-			//?에 바인딩 할 값이 있으면 바인딩한다.
+			//? 에 바인딩 할 값이 있으면 바인딩한다.
 			pstmt.setString(1, dto.getEmail());
-			pstmt.setString(2, dto.getId());
-			//sql 문 수행하고 update or insert or delete 된 row 의 갯수 리턴받기
+			pstmt.setString(2, dto.getProfile());
+			pstmt.setString(3, dto.getId());
+			//sql  문 수행하고 update or insert or delete 된 row 의 갯수 리턴받기 
 			flag = pstmt.executeUpdate();
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -80,7 +82,7 @@ public class UsersDao {
 					pstmt.close();
 				if (conn != null)
 					conn.close();
-			} catch (Exception e2) {
+			} catch (Exception e) {
 			}
 		}
 		if (flag > 0) {
@@ -89,6 +91,7 @@ public class UsersDao {
 			return false;
 		}
 	}
+	
 	//비밀번호를 수정 반영하는 메소드
 	public boolean updatePwd(UsersDto dto) {
 		Connection conn = null;
@@ -96,16 +99,16 @@ public class UsersDao {
 		int flag = 0;
 		try {
 			conn = new DbcpBean().getConn();
-			//실행할 sql 문 준비하기
+			//실행할 sql 문 준비하기 
 			String sql = "UPDATE users"
 					+ " SET pwd=?"
 					+ " WHERE id=? AND pwd=?";
 			pstmt = conn.prepareStatement(sql);
-			//?에 바인딩 할 값이 있으면 바인딩한다.
+			//? 에 바인딩 할 값이 있으면 바인딩한다.
 			pstmt.setString(1, dto.getNewPwd());
 			pstmt.setString(2, dto.getId());
 			pstmt.setString(3, dto.getPwd());
-			//sql 문 수행하고 update or insert or delete 된 row 의 갯수 리턴받기
+			//sql  문 수행하고 update or insert or delete 된 row 의 갯수 리턴받기 
 			flag = pstmt.executeUpdate();
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -115,7 +118,7 @@ public class UsersDao {
 					pstmt.close();
 				if (conn != null)
 					conn.close();
-			} catch (Exception e2) {
+			} catch (Exception e) {
 			}
 		}
 		if (flag > 0) {
