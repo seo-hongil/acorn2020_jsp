aeaa8b1b60dc1543db69da34214f597da1270b27
hongil <shi8295@naver.com>
2020-07-20 18:27:46 +0900

    WEB07_gallery added

--- Web07_Login/src/test/file/dao/FileDao.java
+++ Web07_Login/src/test/file/dao/FileDao.java
@@ -10,7 +10,7 @@ import test.file.dto.FileDto;
 import test.util.DbcpBean;
 
 public class FileDao {
-	public static FileDao dao;
+	private static FileDao dao;
 	private FileDao() {}
 	public static FileDao getInstance() {
 		if(dao==null) {
@@ -18,38 +18,80 @@ public class FileDao {
 		}
 		return dao;
 	}
-	//인자로 잔달되는 파일 번호에 해당하는 파일 정보를 삭제하는 메소드
-		public boolean delete(int num) {
-			Connection conn = null;
-			PreparedStatement pstmt = null;
-			int flag = 0;
-			try {
-				conn = new DbcpBean().getConn();
-				//실행할 sql 문 준비하기 
-				String sql = "DELETE FROM board_file"
-						+ " WHERE num=?";
-				pstmt = conn.prepareStatement(sql);
-				//? 에 바인딩 할 값이 있으면 바인딩한다.
-				pstmt.setInt(1, num);
-				//sql  문 수행하고 update or insert or delete 된 row 의 갯수 리턴받기 
-				flag = pstmt.executeUpdate();
-			} catch (Exception e) {
-				e.printStackTrace();
-			} finally {
-				try {
-					if (pstmt != null)
-						pstmt.close();
-					if (conn != null)
-						conn.close();
-				} catch (Exception e) {
-				}
+	//전체 row 의 갯수를 리턴해주는 메소드
+	public int getCount() {
+		//전체 row  의 갯수를 담을 지역 변수 
+		int count=0;
+		//필요한 객체의 참조값을 담을 지역변수 만들기 
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			//Connection 객체의 참조값 얻어오기 
+			conn = new DbcpBean().getConn();
+			//ROWNUM 중에서 가장 큰 숫자를 얻어오면 전체 row  의 갯수가 된다. 
+			//혹시 row 가 하나도 없으면 null 이 얻어와 지기때문에  null 인 경우 0 으로 
+			//바꿔 줘야 한다.
+			String sql = "SELECT NVL(MAX(ROWNUM), 0) AS num"
+					+ " FROM board_file";
+			pstmt = conn.prepareStatement(sql);
+			//sql 문에 ? 에 바인딩할 값이 있으면 바인딩하고 
+
+			//select 문 수행하고 결과 받아오기 
+			rs = pstmt.executeQuery();
+			//결과 값 추출하기 
+			if (rs.next()) {
+				count=rs.getInt("num");
 			}
-			if (flag > 0) {
-				return true;
-			} else {
-				return false;
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (Exception e) {
 			}
 		}
+		return count;
+	}
+	
+	//인자로 잔달되는 파일 번호에 해당하는 파일 정보를 삭제하는 메소드
+	public boolean delete(int num) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		int flag = 0;
+		try {
+			conn = new DbcpBean().getConn();
+			//실행할 sql 문 준비하기 
+			String sql = "DELETE FROM board_file"
+					+ " WHERE num=?";
+			pstmt = conn.prepareStatement(sql);
+			//? 에 바인딩 할 값이 있으면 바인딩한다.
+			pstmt.setInt(1, num);
+			//sql  문 수행하고 update or insert or delete 된 row 의 갯수 리턴받기 
+			flag = pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (Exception e) {
+			}
+		}
+		if (flag > 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
 	//인자로 전달되는 파일 번호에 해당하는 파일 정보를 리턴하는 메소드
 	public FileDto getData(int num) {
 		
@@ -62,15 +104,16 @@ public class FileDao {
 			//Connection 객체의 참조값 얻어오기 
 			conn = new DbcpBean().getConn();
 			//실행할 sql 문 준비하기
-			String sql = "SELECT writer, title, orgFileName, saveFileName, fileSize, regdate"
-					+ " From board_file"
+			String sql = "SELECT writer,title,orgFileName,saveFileName,"
+					+ " fileSize,regdate"
+					+ " FROM board_file"
 					+ " WHERE num=?";
 			pstmt = conn.prepareStatement(sql);
 			//sql 문에 ? 에 바인딩할 값이 있으면 바인딩하고 
 			pstmt.setInt(1, num);
 			//select 문 수행하고 결과 받아오기 
 			rs = pstmt.executeQuery();
-			//반복문 돌면서 결과 값 추출하기 
+			//결과 값 추출하기 
 			if(rs.next()) {
 				dto=new FileDto();
 				dto.setNum(num);
@@ -98,7 +141,8 @@ public class FileDao {
 	}
 	
 	//파일 목록을 리턴해주는 메소드
-	public List<FileDto> getList(){
+	public List<FileDto> getList(FileDto dto){
+		//파일 목록을 담을 ArrayList  객체 생성 
 		List<FileDto> list=new ArrayList<>();
 		//필요한 객체의 참조값을 담을 지역변수 만들기 
 		Connection conn = null;
@@ -108,27 +152,31 @@ public class FileDao {
 			//Connection 객체의 참조값 얻어오기 
 			conn = new DbcpBean().getConn();
 			//실행할 sql 문 준비하기
-			String sql = "SELECT num,writer,title,orgFileName,saveFileName,fileSize,regdate"
-					+ " FROM board_file"
-					+ " ORDER BY num DESC";
+			String sql = "SELECT *"
+					+ " FROM"
+					+ "     (SELECT result1.*, ROWNUM AS rnum"
+					+ "      FROM (SELECT num,writer,title,orgFileName,fileSize,regdate"
+					+ "            FROM board_file"
+					+ "            ORDER BY num DESC) result1)"
+					+ " WHERE rnum BETWEEN ? AND ?";
 			pstmt = conn.prepareStatement(sql);
 			//sql 문에 ? 에 바인딩할 값이 있으면 바인딩하고 
-			
+			pstmt.setInt(1, dto.getStartRowNum());
+			pstmt.setInt(2, dto.getEndRowNum());
 			//select 문 수행하고 결과 받아오기 
 			rs = pstmt.executeQuery();
 			//반복문 돌면서 결과 값 추출하기 
 			while (rs.next()) {
-				FileDto dto=new FileDto();
-				dto.setNum(rs.getInt("num"));
-				dto.setWriter(rs.getString("writer"));
-				dto.setTitle(rs.getString("title"));
-				dto.setOrgFileName(rs.getString("orgFileName"));
-				dto.setSaveFileName(rs.getString("saveFileName"));
-				dto.setFileSize(rs.getLong("fileSize"));
-				dto.setRegdate(rs.getString("regdate"));
-				//ArrayList 객체에 누적 시킨다.
-				list.add(dto);
-				
+				//select 된 파일의 정보를 FileDto 객체에 담고 
+				FileDto tmp=new FileDto();
+				tmp.setNum(rs.getInt("num"));
+				tmp.setWriter(rs.getString("writer"));
+				tmp.setTitle(rs.getString("title"));
+				tmp.setOrgFileName(rs.getString("orgFileName"));
+				tmp.setFileSize(rs.getLong("fileSize"));
+				tmp.setRegdate(rs.getString("regdate"));
+				//ArrayList 객체에 누적 시킨다. 
+				list.add(tmp);
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -145,6 +193,7 @@ public class FileDao {
 		}
 		return list;
 	}
+	
 	//파일 정보를 DB 에 저장하는 메소드
 	public boolean insert(FileDto dto) {
 		Connection conn = null;
@@ -152,18 +201,18 @@ public class FileDao {
 		int flag = 0;
 		try {
 			conn = new DbcpBean().getConn();
-			//실행할 sql 문 준비하기
+			//실행할 sql 문 준비하기 
 			String sql = "INSERT INTO board_file"
 					+ " (num,writer,title,orgFileName,saveFileName,fileSize,regdate)"
 					+ " VALUES(board_file_seq.NEXTVAL, ?, ?, ?, ?, ?, SYSDATE)";
 			pstmt = conn.prepareStatement(sql);
-			//?에 바인딩 할 값이 있으면 바인딩한다.
+			//? 에 바인딩 할 값이 있으면 바인딩한다.
 			pstmt.setString(1, dto.getWriter());
 			pstmt.setString(2, dto.getTitle());
 			pstmt.setString(3, dto.getOrgFileName());
 			pstmt.setString(4, dto.getSaveFileName());
-			pstmt.setLong(5, dto.getFileSize()); //long type
-			//sql 문 수행하고 update or insert or delete 된 row 의 갯수 리턴받기
+			pstmt.setLong(5, dto.getFileSize()); //long type 
+			//sql  문 수행하고 update or insert or delete 된 row 의 갯수 리턴받기 
 			flag = pstmt.executeUpdate();
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -173,7 +222,7 @@ public class FileDao {
 					pstmt.close();
 				if (conn != null)
 					conn.close();
-			} catch (Exception e2) {
+			} catch (Exception e) {
 			}
 		}
 		if (flag > 0) {
